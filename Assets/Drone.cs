76096e4d7d5ed0d8ca40ee63642f6fe6e206af73
Nurin <nurin_24006493@utp.edu.my>
2024-10-12 03:59:59 +0800

    Add project files.

---
+++ Assets/Drone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Drone : MonoBehaviour
+{
+    public int Temperature { set; get; } = 0;
+
+
+    Flock agentFlock;
+    public Flock AgentFlock { get { return agentFlock; } }
+
+    Collider2D agentCollider;
+    public Collider2D AgentCollider { get { return agentCollider; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentCollider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        Temperature = (int) (Random.value * 100);
+    }
+
+    public void Initialize(Flock flock)
+    {
+        agentFlock = flock;
+    }
+
+    public void Move(Vector2 velocity)
+    {
+        transform.up = velocity;
+        transform.position += (Vector3)velocity * Time.deltaTime;
+    }
+}
